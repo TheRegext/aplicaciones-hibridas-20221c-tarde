f0ca6a65d9dd3abe6641cc37b0bd581ec1220d95
Brian Lara <brian.lara@outlook.com.ar>
2022-05-24 18:00:07 -0300

    react 01

---
+++ Clase #20/front/src/components/Contador.jsx
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+// RENDERIZADO
+function Contador({increment}){
+    let [count, setCount] = useState(0)
+
+    function handleAdd(event){
+        event.preventDefault();
+
+        console.log("Incremente!", count)
+        setCount(count + increment)
+    }
+    
+    return (
+        <div>
+            Valor: {count}
+            <button onClick={handleAdd}>+</button>
+        </div>
+    )
+}
+
+export default Contador
